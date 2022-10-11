bc3da0b7f8521727d90cb997c28d3b750fab9b51
Carlos Bello <114359972+cbello6196@users.noreply.github.com>
2022-10-11 16:26:48 -0600

    Main Menú

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,35 @@
 
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-       
+        Scanner s = new Scanner(System.in);
+        Bancos b = new Bancos();
+        while (true) {
+            System.out.println("\nIngrese una opcion:");
+            System.out.println("1.Agregar Cliente");
+            System.out.println("2. Atender Cliente");
+            System.out.println("3. Ver Clientes");
+            System.out.println("4. Salir");
+            int opcion = s.nextInt();
+            switch (opcion) {
+                case 1:
+                    b.agregar();
+                    break;
+                case 2:
+                    b.atender();
+                    break;
+                case 3:
+                    b.verClientes();
+                    break;
+                case 4:
+                    return;
+                default:
+                    System.out.println("Opcion no valida");
+            }
+
+        }
+
     }
-}    
+}   
