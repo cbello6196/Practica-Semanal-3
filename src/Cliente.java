ca77ba2e2728ce9996646a630dbd6d05a21bc035
Carlos Bello <114359972+cbello6196@users.noreply.github.com>
2022-10-11 16:31:02 -0600

    Clase Cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,2 +1,17 @@
 
+public class Cliente {
+    private String nombre;
+    private String cedula;
+    boolean discapacidad;
+
+    public Cliente(String nombre,String cedula,boolean discapacidad){
+        this.nombre=nombre;
+        this.cedula=cedula;
+        this.discapacidad=discapacidad;
+    }
+    
+    public String toString(){
+        return "Nombre: "+nombre+"\tCedula: "+cedula+"\tDiscapacidad: "+discapacidad;
+    }
+}
 
